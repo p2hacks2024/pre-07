5ee950dc024fd99bb7011799c5b0aa1f65bb6438
Accord33 <skb.ceg.biblia@gmail.com>
2024-12-12 02:32:31 +0900

    footerの完成

--- frontend/front/app.vue
+++ frontend/front/app.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+    <div>
+        <NuxtPage />
+        <Footer class="footer" />
+    </div>
 </template>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+}
+</style>
---
+++ frontend/front/components/footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <a href="#">
+            <img src="@/assets/home.png" alt="home" />
+        </a>
+        <a href="#">
+            <img src="@/assets/folder.png" alt="folder" />
+        </a>
+        <a href="#">
+            <img src="@/assets/search.png" alt="search" />
+        </a>
+        <a href="#">
+            <img src="@/assets/brain.png" alt="brain" />
+        </a>
+        <a href="#">
+            <img src="@/assets/human.png" alt="human" />
+        </a>
+    </div>
+</template>
+<style scoped>
+div {
+    display: flex;
+    background-color: #fff;
+    border-top: 1px solid #A2A2A2;
+    justify-content: space-around;
+    padding: 15px 30px 15px 30px;
+    position: fixed;
+    bottom: 0px;
+    width: 85%;
+    z-index: 1000;
+}
+
+img {
+    width: 30px;
+}
+</style>
