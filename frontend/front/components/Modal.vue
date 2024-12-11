3327186a812f3a4e4fa0cad1eb4ec0e15fb2c4ce
{ttMOCHItt} <{daichi.tnmt@gmail.com}>
2024-12-11 21:43:41 +0900

    コンポーネントいっぱい追加した

---
+++ frontend/front/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="visible" class="modal-backdrop" @click="close">
+    <div class="modal" @click.stop>
+      <button class="modal-close" @click="close">×</button>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+});
+defineEmits(['close']);
+
+const close = () => {
+  emit('close');
+};
+</script>
+
+<style scoped>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+}
+.modal {
+  background: white;
+  padding: 1.5em;
+  border-radius: 8px;
+  width: 90%;
+  max-width: 500px;
+  position: relative;
+}
+.modal-close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: none;
+  background: transparent;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+</style>
---
+++ frontend/front/pages/[account]/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{post}}
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get(
+      `http://10.124.75.43:8000/api/ideas/{params.id}`
+    );
+    return { post: data };
+  },
+}
+</script>
