e9fd12363503872d33b945f5a498e8a9f28e145d
yukari <gmyukari@gmail.com>
2024-12-12 21:31:28 +0900

    検索画面つくった

---
+++ frontend/front/pages/search.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="topText">
+            <p>他の人のアイディアを<br>
+            見てみましょう</p><br>
+        </div>
+    <div class="search">
+        <input type="test" placeholder="何について聞きますか？">
+    </div>
+</template>
+
+
+<style scoped>
+p{
+    text-align: center;
+}
+.topText{
+    padding: 50px;
+    margin-top: 50px;
+}
+.search {
+    text-align: center;
+
+}
+.search input {
+    font-size: 16px;
+    border: 1px solid #fff;
+    background-color: rgb(224, 224, 224);
+    padding: 15px 30px;
+    border-radius: 15px;
+    width: 60%;
+}
+</style>
