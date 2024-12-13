0dbdf266e5dd4882145f877ab065af022c587350
{ttMOCHItt} <{daichi.tnmt@gmail.com}>
2024-12-13 19:08:38 +0900

    textBoxのコンポーネントを追加

---
+++ frontend/front/pages/result.vue
@@ -0,0 +1,40 @@
+<script>
+import PostView from '@/components/PostView.vue'
+import { endpoint } from '~/components/APIEndPoint';
+
+export default {
+    data() {
+        return {
+            ids: []
+        }
+    },
+    async created() {
+        const response = await fetch(endpoint + 'ideas/')
+        const data = await response.json()
+        this.ids = data
+    },
+}
+</script>
+
+<template>
+    <div class="timeLine">
+        <div v-for="id in ids" :key="id" class="postContents">
+            <PostView class="postView" :id="id"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.timeLine {
+    padding: 10px;
+    background-color: #f3f3f3;
+    display:flex;
+    flex-wrap:wrap;
+}
+.postContents {
+    width: 50%;
+}
+.postView {
+    margin: 5%;
+}
+</style>
--- frontend/front/pages/search.vue
+++ frontend/front/pages/search.vue
@@ -1,15 +1,18 @@
+<script>
+import textBox from '~/components/textBox.vue'
+</script>
+
 <template>
     <div class="topText">
             <p>他の人のアイディアを<br>
             見てみましょう</p><br>
         </div>
-    <div class="search">
-        <input type="test" placeholder="何について聞きますか？">
-    </div>
+        <textBox textMessage="アイデアを検索"></textBox>
 </template>
 
 
 <style scoped>
+
 p{
     text-align: center;
 }
@@ -17,16 +20,5 @@ p{
     padding: 50px;
     margin-top: 50px;
 }
-.search {
-    text-align: center;
 
-}
-.search input {
-    font-size: 16px;
-    border: 1px solid #fff;
-    background-color: rgb(224, 224, 224);
-    padding: 15px 30px;
-    border-radius: 15px;
-    width: 60%;
-}
 </style>
