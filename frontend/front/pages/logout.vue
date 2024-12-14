e89aeb22317f3d11bfbfe465b448421f0a4f7c28
{ttMOCHItt} <{daichi.tnmt@gmail.com}>
2024-12-15 00:55:45 +0900

    ログアウト機能、ログイン画面遷移の実装

---
+++ frontend/front/pages/logout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>ログアウト</h1>
+        <input type="button" @click="logout" value="ログアウト">
+    </div>
+</template>
+
+<script>
+import { endpoint } from '~/components/APIEndPoint';
+export default {
+
+    methods: {
+        async logout() {
+            const response = await fetch(endpoint+'logout', {
+                method: 'POST',
+                credentials: 'include'
+            });
+            if (response.ok) {
+              console.log('Logout success');
+                this.$router.push('/login');
+            } else {
+                console.error('Logout failed');
+            }
+        }
+    }
+}
+</script>
