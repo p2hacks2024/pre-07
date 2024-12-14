a1a32e981e84cd538cd5614517d694742f8eabdc
Accord33 <skb.ceg.biblia@gmail.com>
2024-12-15 01:55:01 +0900

    多分PWA対応のせいでプロジェクトが破壊されたので、もう一度プロジェクトを再生成

--- frontend/front/app.vue
+++ frontend/front/app.vue
@@ -1,19 +1,8 @@
-<script setup lang="ts"></script>
 <template>
+
     <div>
         <Header />
-        <div class="space"></div>
-        <NuxtPwaManifest />
         <NuxtPage />
-        <Footer class="footer" />
+        <Footer />
     </div>
 </template>
-<style>
-body {
-    margin: 0;
-    padding: 0;
-}
-.space {
-    margin-top: 80px;
-}
-</style>
